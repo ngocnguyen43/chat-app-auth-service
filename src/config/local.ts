9c42f6980bc98acf99a61c25238167684698cb81
minhngoc <minhngocnguyen.403@gmail.com>
2024-01-07 09:27:44 +0700

    chore: fix tz [#13]

--- src/config/local.ts
+++ src/config/local.ts
@@ -1,8 +1,10 @@
+import * as dotenv from "dotenv"
+dotenv.config()
 export default {
   port: 6001,
   redis_port: process.env.REDIS_PORT_DEV || 1051,
   redis_host: process.env.REDIS_HOST || '127.0.0.1',
   MESSAGE_BROKER_URL: process.env.MESSAGE_BROKER_URL_DEV || 'amqp://127.0.0.1:5672',
-  CONSUL_URL: process.env.CONSUL_HOST_DEV || '127.0.0.1',
+  CONSUL_URL: process.env.CONSUL_HOST || '127.0.0.1',
   HOST_PORT: process.env.HOST_PORT || 6001
 };
